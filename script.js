490a12035c32572ade4f21e507ca2f47136383ce
celsodaniel <140808519+celsodaniel@users.noreply.github.com>
2024-09-20 12:27:59 -0300

    fiz no colegio

--- script.js
+++ script.js
@@ -10,20 +10,39 @@ document.getElementById('toggle-menu').addEventListener('click', function() {
     
 });
 
-document.getElementById('button').addEventListener('click', function() {
-    const button = document.getElementById('button');
+
+const button = document.getElementById('button');
 
         button.addEventListener('click', function() {
             window.location.href = 'tarefas.html';
 
        
-})});
+});
 
-document.getElementById('button-home').addEventListener('click', function() {
-    const button = document.getElementById('button-home');
 
-        button.addEventListener('click', function() {
-            window.location.href = 'tarefas.html';
+const button_home = document.getElementById('button-home');
+
+    button_home.addEventListener('click', function() {
+        window.location.href = 'tarefas.html';
+    });
+
+
+
+
+const volta = document.getElementById('volta');
+
+    volta.addEventListener('click', function() {
+        window.location.href = 'index.html';
 
        
-})});
+});
+//---------------------------------------------------------------------------------------------------------------------
+
+
+const trabalho1 = document.getElementById('trabalho1');
+
+    trabalho1.addEventListener('click', function() {
+        window.location.href = 'trabalho.html'; //mudar o destino
+
+       
+});
