979f424578abd1cf41dc9b64382d24aa7131ccf3
celsodaniel <140808519+celsodaniel@users.noreply.github.com>
2024-10-05 10:37:57 -0300

    terminado basketboll

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('toggle-menu').addEventListener('click', function() {
-    const menu = document.querySelector('#toggle-menu');
+    menu = document.querySelector('#toggle-menu');
     
     if (menu.style.right === '0%') {
         menu.style.right = '-355px';
--- scripttb.js
+++ scripttb.js
@@ -1,4 +1,38 @@
+const tb1 = document.getElementById('trabalhos1');
+const tb2 = document.getElementById('trabalhos2');
+const tb3 = document.getElementById('trabalhos3');
+const tb4 = document.getElementById('trabalhos4');
+const tela = document.getElementById('info');
+
 document.getElementById('tarefas1').addEventListener('click', function(){
-    
+    tela.style.zIndex = '1';
+    tb1.style.zIndex = '2';
+    tb2.style.zIndex = '0';
+    tb3.style.zIndex = '0';
+    tb4.style.zIndex = '0';
+
+});
+document.getElementById('tarefas2').addEventListener('click', function(){
+    tela.style.zIndex = '1';
+    tb1.style.zIndex = '0';
+    tb2.style.zIndex = '2';
+    tb3.style.zIndex = '0';
+    tb4.style.zIndex = '0';
+
+});
+document.getElementById('tarefas3').addEventListener('click', function(){
+    tela.style.zIndex = '1';
+    tb1.style.zIndex = '0';
+    tb2.style.zIndex = '0';
+    tb3.style.zIndex = '2';
+    tb4.style.zIndex = '0';
+
+});
+document.getElementById('tarefas4').addEventListener('click', function(){
+    tela.style.zIndex = '1';
+    tb1.style.zIndex = '0';
+    tb2.style.zIndex = '0';
+    tb3.style.zIndex = '0';
+    tb4.style.zIndex = '2';
 
 });
