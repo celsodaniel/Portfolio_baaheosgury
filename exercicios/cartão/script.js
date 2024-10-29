aa420352fed2fb65f9da82b0785d225e899e0958
celsodaniel <140808519+celsodaniel@users.noreply.github.com>
2024-10-29 18:27:13 -0300

    1

--- exercicios/cartão/script.js
+++ exercicios/cartão/script.js
@@ -10,7 +10,6 @@ document.getElementById('pcelso').addEventListener('click', function() {
     celso.style.left = '35%';
     
 });
-
 document.getElementById('tntl').addEventListener('click', function() {
     leo = document.querySelector('#leonardo');
     
@@ -20,6 +19,36 @@ document.getElementById('tntl').addEventListener('click', function() {
 document.getElementById('tntc').addEventListener('click', function() {
     celso = document.querySelector('#celso');
     
-    celso.style.left = '-30%';
+    celso.style.left = '-40%';
+    
+});
+
+document.getElementById('endereco2').addEventListener('click', function(){
+    alert('PAIS: Brasil, ESTADO: Paraná, CIDADE: Curitiba')
+
+});
+
+document.getElementById('email2').addEventListener('click', function(){
+    alert('leonardo.coutinho.campos@escola.pr.gov.br')
+    
+});
+
+document.getElementById('contato2').addEventListener('click', function(){
+    alert('(41) 99643-XXXX')
+    
+});
+
+document.getElementById('endereco1').addEventListener('click', function(){
+    alert('PAIS: Brasil, ESTADO: Paraná, CIDADE: Almirante-Tamandare')
+
+});
+
+document.getElementById('email1').addEventListener('click', function(){
+    alert('celso.dubiella.costa@escola.pr.gov.br')
+    
+});
+
+document.getElementById('contato1').addEventListener('click', function(){
+    alert('(41) 99962-XXXX')
     
 });
