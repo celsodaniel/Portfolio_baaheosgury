03219f5d1bf8a5836f9b26628270552621858d78
celsodaniel <140808519+celsodaniel@users.noreply.github.com>
2024-10-29 16:52:07 -0300

    1

---
+++ exercicios/cartão/script.js
@@ -0,0 +1,25 @@
+document.getElementById('pleo').addEventListener('click', function() {
+    leo = document.querySelector('#leonardo');
+
+    leo.style.left = '35%';
+
+});
+document.getElementById('pcelso').addEventListener('click', function() {
+    celso = document.querySelector('#celso');
+    
+    celso.style.left = '35%';
+    
+});
+
+document.getElementById('tntl').addEventListener('click', function() {
+    leo = document.querySelector('#leonardo');
+    
+    leo.style.left = '100%';
+    
+});
+document.getElementById('tntc').addEventListener('click', function() {
+    celso = document.querySelector('#celso');
+    
+    celso.style.left = '-30%';
+    
+});
